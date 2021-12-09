ef8fa1b2d4aa6eed76662fb81226bb8421636f5d
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-12-09 22:53:12 +0300

    movie genres has been added

--- app/src/main/java/com/example/movieproject/MovieListFragment.kt
+++ app/src/main/java/com/example/movieproject/MovieListFragment.kt
@@ -64,6 +64,24 @@ class MovieListFragment : Fragment() {
             R.id.action -> viewModel.getMovieFilter(MovieApiFilter.SHOW_ACTION)
             R.id.adventure -> viewModel.getMovieFilter(MovieApiFilter.SHOW_ADVENTURE)
             R.id.animation -> viewModel.getMovieFilter(MovieApiFilter.SHOW_ANIMATION)
+            R.id.Comedy -> viewModel.getMovieFilter(MovieApiFilter.SHOW_COMEDY)
+            R.id.Crime -> viewModel.getMovieFilter(MovieApiFilter.SHOW_CRIME)
+            R.id.Drama -> viewModel.getMovieFilter(MovieApiFilter.SHOW_DRAMA)
+            R.id.Documentary -> viewModel.getMovieFilter(MovieApiFilter.SHOW_DOCUMENTARY)
+            R.id.Family -> viewModel.getMovieFilter(MovieApiFilter.SHOW_FAMILY)
+            R.id.Fantasy -> viewModel.getMovieFilter(MovieApiFilter.SHOW_FANTASY)
+            R.id.History -> viewModel.getMovieFilter(MovieApiFilter.SHOW_HISTORY)
+            R.id.Horror -> viewModel.getMovieFilter(MovieApiFilter.SHOW_HORROR)
+            R.id.Music -> viewModel.getMovieFilter(MovieApiFilter.SHOW_MUSIC)
+            R.id.Mystery -> viewModel.getMovieFilter(MovieApiFilter.SHOW_MYSTERY)
+            R.id.Romance -> viewModel.getMovieFilter(MovieApiFilter.SHOW_ROMANCE)
+            R.id.Science_Fiction -> viewModel.getMovieFilter(MovieApiFilter.SHOW_SCIENCE_FICTION)
+            R.id.TV_Movie -> viewModel.getMovieFilter(MovieApiFilter.SHOW_TV_MOVIE)
+            R.id.Thriller -> viewModel.getMovieFilter(MovieApiFilter.SHOW_THRILLER)
+            R.id.War -> viewModel.getMovieFilter(MovieApiFilter.SHOW_WAR)
+            R.id.Western -> viewModel.getMovieFilter(MovieApiFilter.SHOW_WESTERN)
+
+
             R.id.sort -> viewModel.sort()
 
         }
